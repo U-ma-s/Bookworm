43b473130897c45a48e66dfd43f9299ef5449b5f
U-ma-s <yuma.prodia4989@gmail.com>
2023-05-11 21:40:03 +0900

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,18 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//  
+//  Created by umas on 2023/05/11
+//  
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
